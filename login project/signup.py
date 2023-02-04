d2ed564a79fc23d4785cc22cb04ccb6cf68ccdb6
Gungun Gupta <104621253+Vanya-1314@users.noreply.github.com>
2023-02-05 00:20:37 +0530

    Update signup.py

--- login project/signup.py
+++ login project/signup.py
@@ -1,9 +1,58 @@
 from tkinter import *
+from tkinter import messagebox
 from PIL import ImageTk
+import pymysql
+
+def clear():
+    emailEntry.delete(0,END)
+    usernameEntry.delete(0,END)
+    passwordEntry.delete(0,END)
+    confirmpasswordEntry.delete(0,END)
+    check.set(0)
+    
 def login_page():
     signup_window.destroy()
     import signin
-
+    
+def connect_database():
+    if emailEntry.get()=='' or usernameEntry.get()=='' or passwordEntry.get()=='' or confirmpasswordEntry.get()=='': 
+        messagebox.showerror('Error','All fields are required')
+    elif passwordEntry.get()!=confirmpasswordEntry.get():
+        messagebox.showerror('Error','Password Mismatch')
+    elif check.get()==0:
+        messagebox.showerror('Error','Please accept terms and conditions.')
+    else:
+        try:
+            con=pymysql.connect(host='localhost',user='root',password='gungun1404')
+            mycursor=con.cursor()
+        except:
+            messagebox.showerror('Error','Database Connectivity Issue, Try again!')
+            return
+        try:
+            query='create database userdata'
+            mycursor.execute(query)
+            query='use userdata'
+            mycursor.execute(query)
+            query='create table data(id int auto_increment primary key not null,email varchar(50),username varchar(100),password varchar(20))'
+            mycursor.execute(query)
+        except:
+            mycursor.execute('use userdata')
+        query='select * from data where username=%s'
+        mycursor.execute(query,(usernameEntry.get()))
+        row=mycursor.fetchone()  
+        if row!=None:
+            messagebox.showerror('Error','Username already exists')
+        else:
+            query='insert into data(email,username,password) values(%s,%s,%s)'
+            mycursor.execute(query,(emailEntry.get(),usernameEntry.get(),passwordEntry.get()))
+            con.commit()
+            con.close()
+            messagebox.showinfo('Success','Registration is successfull')
+            clear()
+            signup_window.destroy()
+            import signin
+        
+        
 signup_window=Tk()
 signup_window.title('Signup Page')
 signup_window.resizable(False,False)
@@ -42,10 +91,11 @@ confirmpasswordLabel.grid(row=7,column=0,sticky='w',padx=25,pady=(10,0))
 confirmpasswordEntry=Entry(frame,width=30,font=('Microsoft Yahei Ui Light',10,'bold'),fg='white',bg='firebrick1')
 confirmpasswordEntry.grid(row=8,column=0,sticky='w',padx=25)
 
-terms=Checkbutton(frame,text='I agree to the Terms and Conditions',font=('Microsoft Yahei Ui Light',10,'bold'),fg='firebrick1',bg='white',activeforeground='firebrick1',activebackground='white',cursor='hand2')
+check=IntVar()
+terms=Checkbutton(frame,text='I agree to the Terms and Conditions',font=('Microsoft Yahei Ui Light',10,'bold'),fg='firebrick1',bg='white',activeforeground='firebrick1',activebackground='white',cursor='hand2',variable=check)
 terms.grid(row=9,column=0,padx=10,pady=15)
 
-signupButton=Button(frame,text='Signup',font=('Open Sans',16,'bold'),bd=0,bg='firebrick1',fg='white',activeforeground='white',activebackground='firebrick1',width=17)
+signupButton=Button(frame,text='Signup',font=('Open Sans',16,'bold'),bd=0,bg='firebrick1',fg='white',activeforeground='white',activebackground='firebrick1',width=17,command=connect_database)
 signupButton.grid(row=10,column=0,padx=10)
 
 alreadyaccount=Label(frame,text="Don't have an account?",font=('Open Sans',9,'bold'),bg='white',fg='firebrick1')
@@ -53,8 +103,5 @@ alreadyaccount.grid(row=11,column=0,sticky='w',padx=25,pady=10)
 
 LoginButton=Button(frame,text="Login",font=('Open Sans',9,'bold underline'),bg='white',fg='blue',cursor='hand2',bd=0,activebackground='white',activeforeground='blue',command=login_page)
 LoginButton.place(x=170,y=397)
-
-
-
 
 signup_window.mainloop()
